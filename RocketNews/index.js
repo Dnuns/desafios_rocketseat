60d448abdf76c211d1d4a82bd1a3e5b49fb5efe2
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-04-29 17:57:39 -0100

    Update index.js

--- RocketNews/index.js
+++ RocketNews/index.js
@@ -1,4 +1,5 @@
 document.querySelector('.send-btn-container').onclick = () => {
     let email = document.getElementById('email').value;
+    let allEmails = []
     alert(`O e-mail "${email}" foi inserido com sucesso!`)
 }
