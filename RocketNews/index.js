7d25ca682557705efbfe7b904a726644e5c57353
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-04-29 21:31:04 -0100

    add email validator function

--- RocketNews/index.js
+++ RocketNews/index.js
@@ -1,5 +1,21 @@
-document.querySelector('.send-btn-container').onclick = () => {
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+document.querySelector('.send-btn-container').onclick = (e) => {
+
     let email = document.getElementById('email').value;
-    let allEmails = []
+
+    if (!validateEmail(email)) {
+        alert('Por favor insira um e-mail válido!')
+        return
+    }
+
     alert(`O e-mail "${email}" foi inserido com sucesso!`)
+
+    e.preventDefault()
 }
