2a17869b64e1ea05bfce6767a584e99db24e6237
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-04-29 17:51:10 -0100

    add alert email function

---
+++ RocketNews/index.js
@@ -0,0 +1,4 @@
+document.querySelector('.send-btn-container').onclick = () => {
+    let email = document.getElementById('email').value;
+    alert(`O e-mail "${email}" foi inserido com sucesso!`)
+}
