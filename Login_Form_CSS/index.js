b5a347c82952034e72ce4f6388d3ad1c66119e71
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-12-29 17:58:24 -0100

    feat: add email input

---
+++ Login_Form_CSS/index.js
@@ -0,0 +1,18 @@
+//Change e-mail icon on focus
+/*
+let emailInput = document.querySelector('#email');
+
+let formSection = document.querySelector('.form-section');
+
+let emailIcon = document.querySelector(".has-icon");
+
+
+formSection.addEventListener('click', () => {
+    emailIcon.classList.replace('icon-focus', 'icon');
+});
+
+emailInput.addEventListener('focus', () => {
+
+    emailIcon.classList.replace('icon', 'icon-focus');
+});
+*/
