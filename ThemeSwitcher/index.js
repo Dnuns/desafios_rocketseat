c3b0d7f30102715ef72fa24fb210c5952f8c694e
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-01-16 13:22:27 -0100

    feat: save theme on local storage

--- ThemeSwitcher/index.js
+++ ThemeSwitcher/index.js
@@ -1,4 +1,21 @@
-var ball = document.querySelector('.ball').addEventListener('click', () => {
+//Dark mode switcher
+
+document.querySelector('.ball').addEventListener('click', () => {
+
+
+
+
     document.querySelector('.ball').classList.toggle('active');
-    document.querySelector('.main').classList.toggle('dark');
+
+    let darkTheme = document.querySelector('.main').classList.toggle('dark');
+
+    console.log(localStorage.setItem('theme', JSON.stringify(darkTheme)));
+
 });
+
+let getTheme = JSON.parse(localStorage.getItem('theme'));
+
+if (getTheme === true) {
+    document.querySelector('.main').classList.add('dark');
+    document.querySelector('.ball').classList.toggle('active');
+}
